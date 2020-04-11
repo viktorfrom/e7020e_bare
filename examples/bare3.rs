940f55ffc91855e39c9a36daea3ea49ce357e0ec
viktorfrom <viktor.from91@gmail.com>
2020-04-11 17:28:04 +0200

    Update bare3.rs

--- examples/bare3.rs
+++ examples/bare3.rs
@@ -18,26 +18,26 @@ use cortex_m_semihosting::{hprint, hprintln};
 #[entry]
 fn main() -> ! {
     hprintln!("bare3").unwrap();
-    let s = "ABCD";
-    let bs = s.as_bytes();
+    let s: &str = "ABCD";
+    let bs: &[u8] = s.as_bytes();
 
     hprintln!("s = {}", s).unwrap();
     hprintln!("bs = {:?}", bs).unwrap();
 
     hprintln!("iterate over slice").unwrap();
     for c in bs {
-        hprint!("{},", c).unwrap();
+        hprint!("{},", c as &u8).unwrap();
     }
 
     hprintln!("iterate iterate using (raw) indexing").unwrap();
-    for i in 0..s.len() {
+    for i in 0usize..s.len() {
         hprintln!("{},", bs[i]).unwrap();
     }
 
     hprintln!("").unwrap();
 
-    let a = [65u8; 4];
-    // let mut a = [0u8; 4];
+    //let a: [u8; 4] = [65u8; 4];
+    let mut a = [0u8; 4];
 
     hprintln!("").unwrap();
     hprintln!("a = {}", core::str::from_utf8(&a).unwrap()).unwrap();
@@ -90,7 +90,7 @@ fn main() -> ! {
 //    What is the type of `i`?
 //
 //    ** your answer here **
-//    answer: i32
+//    answer: usize
 //
 //    Commit your answers (bare3_1)
 //
